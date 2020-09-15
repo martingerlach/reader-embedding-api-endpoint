171c48f0943ab38e3fc8e4bdcef3bf60c9bbacbe
Martin Gerlach <mgerlach@wikimedia.org>
2020-09-15 13:38:18 +0200

    Update cloudvps_setup.sh

--- model/config/cloudvps_setup.sh
+++ model/config/cloudvps_setup.sh
@@ -4,7 +4,7 @@
 # these can be changed but most other variables should be left alone
 APP_LBL='api-endpoint'  # descriptive label for endpoint-related directories
 REPO_LBL='reader-embedding'  # directory where repo code will go
-GIT_CLONE_HTTPS='https://github.com/martingerlach/test-api.git'  # for `git clone`
+GIT_CLONE_HTTPS='https://github.com/martingerlach/reader-embedding-api-endpoint.git'  # for `git clone`
 # MODEL_WGET='https://ndownloader.figshare.com/files/<file-number>'  # model binary -- ndownloader.figshare is a good host
 
 ETC_PATH="/etc/${APP_LBL}"  # app config info, scripts, ML models, etc.
